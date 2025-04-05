802a2f5fae7a1c8054b8338e7e1e68cff971f980
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-04-05 16:34:49 +0200

    Removed results-page-component and added quiz-results and quiz-game to the quiz-page-component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { LoginPageComponent } from './components/login-page/login-page.component
 import { RegisterPageComponent } from './components/register-page/register-page.component';
 import { MainMenuPageComponent } from './components/main-menu-page/main-menu-page.component';
 import { ImpressumPageComponent } from './components/impressum-page/impressum-page.component';
-import { ResultsPageComponent } from './components/results-page/results-page.component';
 import { HighscoresPageComponent } from './components/highscores-page/highscores-page.component';
 import { QuizPageComponent } from './components/quiz-page/quiz-page.component';
 import { AdminPanelComponent } from './components/admin-panel/admin-panel.component';
@@ -13,7 +12,6 @@ export const routes: Routes = [
     {path: "register", component: RegisterPageComponent},
     {path: "main-menu", component: MainMenuPageComponent},
     {path: "quiz", component: QuizPageComponent},
-    {path: "results", component: ResultsPageComponent},
     {path: "admin-panel", component: AdminPanelComponent},
     {path: "highscores", component: HighscoresPageComponent},
     {path: "impressum", component: ImpressumPageComponent},
--- frontend/src/app/components/quiz-page/quiz-page.component.ts
+++ frontend/src/app/components/quiz-page/quiz-page.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { QuizSelectionComponent } from './quiz-selection/quiz-selection.component';
+import { QuizGameComponent } from './quiz-game/quiz-game.component';
+import { QuizResultsComponent } from './quiz-results/quiz-results.component';
 
 @Component({
   selector: 'app-quiz-page',
-  imports: [QuizSelectionComponent],
+  imports: [QuizSelectionComponent, QuizGameComponent, QuizResultsComponent],
   templateUrl: './quiz-page.component.html',
   styleUrl: './quiz-page.component.css',
 })
 export class QuizPageComponent {
   quizStarted: boolean = false;
+  quizFinished: boolean = false;
 
   startQuiz() {
     this.quizStarted = true;
+  }
+
+  finishQuiz() {
+    this.quizFinished = true;
   }
 }
