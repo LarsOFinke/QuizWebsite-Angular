a83757ed7cbcccf5445d292dea7a052775586afd
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-04-05 17:20:54 +0200

    Quiz-selection question-amount-slider functional now

--- frontend/src/app/components/quiz-page/quiz-selection/quiz-selection.component.ts
+++ frontend/src/app/components/quiz-page/quiz-selection/quiz-selection.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-quiz-selection',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './quiz-selection.component.html',
   styleUrl: './quiz-selection.component.css',
 })
 export class QuizSelectionComponent {
-  updateAmountLabel() {}
+  questionAmount: number = 10;
+
+  onRangeChange(newValue: number) {
+    this.questionAmount = newValue;
+  }
 
   updateTopics() {}
 
