87530dfe75355fcbec0cef14c3aaff07af1d763b
Lars-Oliver Finke <dev.Lars-Finke@web.de>
2025-04-07 12:19:38 +0200

    Transfered HTML and CSS for quiz-results and -game

--- frontend/src/app/components/quiz-page/quiz-results/quiz-results.component.ts
+++ frontend/src/app/components/quiz-page/quiz-results/quiz-results.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-quiz-results',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './quiz-results.component.html',
-  styleUrl: './quiz-results.component.css'
+  styleUrl: './quiz-results.component.css',
 })
-export class QuizResultsComponent {
-
-}
+export class QuizResultsComponent {}
